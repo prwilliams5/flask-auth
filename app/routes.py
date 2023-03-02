2efe8b3135816a527de61a34f23bc2b3979b7fa1
parker <prwilliams5@gmail.com>
2023-03-01 20:48:45 -0600

    added user template & route

--- app/routes.py
+++ app/routes.py
@@ -58,6 +58,16 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    comments = [
+        {'author': user, 'body': 'test 1'},
+        {'author': user, 'body': 'test 2'}
+    ]
+    return render_template('user.html', user=user, comments=comments)
+
     # racks = [
     #     {  
     #         'id': {
